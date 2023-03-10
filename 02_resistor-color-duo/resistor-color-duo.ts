37e44d8462de4e19a570e49a4a5d9a2696207186
Clémence Bergon <clem.bergon@gmail.com>
2023-03-10 13:51:56 +0100

    resistor-color-duo https://exercism.org/tracks/typescript/exercises/resistor-color-duo

---
+++ 02_resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,16 @@
+const COLORS: Record<string, number> = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export function decodedValue(colors: string[]): number {
+  return COLORS[colors[0]] * 10 + COLORS[colors[1]];
+}
